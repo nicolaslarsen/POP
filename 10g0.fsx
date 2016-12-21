940a7d2dcb2500276eafe1c7e0d2af1ac91ecc79
kasper <nicolasringsmose@hotmail.com>
2016-12-21 02:22:08 +0100

    fixed some bugs and added carni- and herbivore

--- 10g0.fsx
+++ 10g0.fsx
@@ -3,44 +3,78 @@ open System
 // Randomizer
 let rand = new System.Random()
             
-type Animal(weight : int, max_speed : int) =
-    class
-        let mutable current_speed = 0;
-        let mutable daily_required_intake = 0;
-        let mutable intake_percent = 0;
-
-        member this.daily_intake = daily_required_intake
-        member this.intake_prct = intake_percent
-        member this.weight = weight
-        member this.max_speed = max_speed
-        member this.curr_speed = current_speed
+type Animal(weight, max_speed) = class
+    let mutable current_speed = 0.0
+    let mutable daily_required_intake = 0.0
+    let mutable intake_percent = 0.0
         
-        // Constructor with speed only, generates random weight between 70 and 300
-        new(max_speed) =
-            let weight = rand.Next(70,300)
-            Animal(weight, max_speed)
+    member this.daily_intake
+        with get() = daily_required_intake
+        and set(value) = daily_required_intake <- value
+    member this.intake_prct 
+        with get() = intake_percent
+        and set(value) = intake_percent <- value
+    member this.weight = weight
+    member this.max_speed = max_speed
+    member this.curr_speed = current_speed
+    
+    // Constructor with speed only, generates random weight between 70 and 300
+    new(max_speed) =
+        let weight = float(rand.Next(70,300))
+        Animal(weight, max_speed)
+        
+    // Set current speed based on food eat eaten with respect to food needed
+    abstract member setSpeed: unit -> unit
+    default this.setSpeed() =
+        current_speed <- max_speed/100.0 * this.intake_prct
 
-        // Set current speed based on food eat eaten with respect to food needed
-        member this.setSpeed() =
-            current_speed <- max_speed/100 * this.intake_prct
+    // Set a required daily intake with respect to the weight of the animal
+    abstract member setIntake: unit -> unit
+    default this.setIntake() =
+        daily_required_intake <- weight/2.0
 
-        // Set a required daily intake with respect to the weight of the animal
-        member this.setIntake() =
-            daily_required_intake <- weight/2
+    // For testing purposes
+    member this.print_attributes() =
+        printfn "Amount of daily food needed: %f" this.daily_intake
+        printfn "Percent of needed intake eaten: %f" this.intake_prct
+        printfn "Weight: %f" this.weight
+        printfn "Max speed: %f" this.max_speed
+        printfn "Current speed: %f" this.curr_speed
+end
 
-        // For testing purposes
-        member this.print_attributes() =
-            printfn "Amount of daily food needed: %d" this.daily_intake
-            printfn "Percent of needed intake eaten: %d" this.intake_prct
-            printfn "Weight: %d" this.weight
-            printfn "Max speed: %d" this.max_speed
-            printfn "Current speed: %d" this.curr_speed
-    end
+type Carnivore(weight , speed) = class
+    inherit Animal(weight, speed)
+
+    // Carnivores only need to eat 8% of their weight per day
+    override this.setIntake() =
+        this.daily_intake <- weight/100.0 * 8.0
+end
+
+type Herbivore(weight, speed) = class
+    inherit Animal(weight, speed)
+
+    // Herbivores need to eat 40% of their weight per day
+    override this.setIntake() =
+        this.daily_intake <- weight/100.0 * 40.0
+end
 
 let main() =
-    let memes = new Animal(100, 50)
+    let memes = new Animal(100.0, 50.0)
+    let cheetah = new Carnivore(50.0, 114.0)
+    let antelope = new Herbivore(50.0, 95.0)
+    let wildebeest = new Herbivore(200.0, 80.0)
+    
     memes.setSpeed()
     memes.setIntake()
+    cheetah.setSpeed()
+    cheetah.setIntake()
+    antelope.setSpeed()
+    antelope.setIntake()
+    wildebeest.setSpeed()
+    wildebeest.setIntake()
     memes.print_attributes()
+    cheetah.print_attributes()
+    antelope.print_attributes()
+    wildebeest.print_attributes()
     
 main()
