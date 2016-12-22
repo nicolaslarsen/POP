9ce038ff2091a7a06e25d643b93439d290de37ae
derp <nicolasringsmose@hotmail.com>
2016-12-22 22:24:19 +0100

    rest of 10g

--- 10g0.fsx
+++ 10g0.fsx
@@ -11,9 +11,11 @@ type Animal(weight, max_speed) = class
     member this.daily_intake
         with get() = daily_required_intake
         and set(value) = daily_required_intake <- value
+        
     member this.intake_prct 
         with get() = intake_percent
         and set(value) = intake_percent <- value
+        
     member this.weight = weight
     member this.max_speed = max_speed
     member this.curr_speed = current_speed
@@ -59,22 +61,97 @@ type Herbivore(weight, speed) = class
 end
 
 let main() =
-    let memes = new Animal(100.0, 50.0)
-    let cheetah = new Carnivore(50.0, 114.0)
-    let antelope = new Herbivore(50.0, 95.0)
-    let wildebeest = new Herbivore(200.0, 80.0)
-    
-    memes.setSpeed()
-    memes.setIntake()
-    cheetah.setSpeed()
-    cheetah.setIntake()
-    antelope.setSpeed()
-    antelope.setIntake()
-    wildebeest.setSpeed()
-    wildebeest.setIntake()
-    memes.print_attributes()
-    cheetah.print_attributes()
-    antelope.print_attributes()
-    wildebeest.print_attributes()
-    
+    let mutable winner = false
+    while(not winner) do
+        let mutable cheetah_average    = 0.0
+        let mutable antelope_average   = 0.0
+        let mutable wildebeest_average = 0.0
+        for i in 1..3 do
+            printfn "-----------------------------------------------------------------------"
+            printfn "Round: %d" i
+            printfn "-----------------------------------------------------------------------\n"
+            let cheetah = new Carnivore(50.0, 114.0)
+            let antelope = new Herbivore(50.0, 95.0)
+            let wildebeest = new Herbivore(200.0, 80.0)
+            cheetah.intake_prct    <- float(rand.Next(1,100))
+            antelope.intake_prct   <- float(rand.Next(1,100))
+            wildebeest.intake_prct <- float(rand.Next(1,100))
+            
+            cheetah.setIntake()
+            cheetah.setSpeed()
+            antelope.setIntake()
+            antelope.setSpeed()
+            wildebeest.setIntake()
+            wildebeest.setSpeed()
+            
+            // Get the times in minutes
+            let cheetah_time = 10.0/cheetah.curr_speed * 60.0
+            let antelope_time = 10.0/antelope.curr_speed * 60.0
+            let wildebeest_time = 10.0/wildebeest.curr_speed * 60.0
+
+            // Printing
+            printfn "Cheetah"
+            printfn "intake percentage: %2.2f%%" cheetah.intake_prct
+            printfn "actual intake: %2.2fkg" (cheetah.daily_intake/100.0 * cheetah.intake_prct)
+            printfn "required intake: %2.2fkg" cheetah.daily_intake 
+            printfn "time to cover 10km: %2.2f minutes\n" cheetah_time
+            cheetah_average <- cheetah_average + cheetah_time
+            
+            printfn "Antelope"
+            printfn "intake percentage: %2.2f%%" antelope.intake_prct
+            printfn "actual intake: %2.2fkg" (antelope.daily_intake/100.0 * antelope.intake_prct)
+            printfn "required intake: %2.2fkg" antelope.daily_intake 
+            printfn "time to cover 10km: %2.2f minutes\n" antelope_time
+            antelope_average <- antelope_average + antelope_time
+            
+            printfn "Wildebeest"
+            printfn "intake percentage: %.2f%%" wildebeest.intake_prct
+            printfn "actual intake: %.2fkg" (wildebeest.daily_intake/100.0 * wildebeest.intake_prct)
+            printfn "required intake: %.2fkg" wildebeest.daily_intake 
+            printfn "time to cover 10km: %.2f minutes\n" wildebeest_time
+            wildebeest_average <- wildebeest_average + wildebeest_time
+
+        // Calculate averages
+        cheetah_average    <- cheetah_average / 3.0
+        antelope_average   <- antelope_average / 3.0
+        wildebeest_average <- wildebeest_average / 3.0
+
+        // Small function to find the fastest animal
+        let findFastest (x,xname) (y,yname) (z,zname) =
+            let mutable fastest = x
+            let mutable name = xname
+            
+            // find the fastest time
+            if y < fastest then
+                fastest <- y
+                name <- yname
+            if z < fastest then
+                fastest <- z
+                name <- zname
+            (fastest, name)
+
+        // Small function that checks
+        // if there are any animals as fast as the fastest
+        let checkFastest fastest x y z =
+            let mutable numFastest = 0
+            if x = fastest then
+                numFastest <- numFastest + 1
+            if y = fastest then
+                numFastest <- numFastest + 1
+            if z = fastest then
+                numFastest <- numFastest + 1
+            // Checks if there is indeed only 1 fastest animal
+            numFastest = 1
+        
+        
+        let (time, winnerName) = findFastest (cheetah_average, "Cheetah") (antelope_average, "Antelope") (wildebeest_average, "Wildebeest")
+        winner <- checkFastest time cheetah_average antelope_average wildebeest_average
+        if winner then
+            printfn "-----------------------------------------------------------------------"
+            printfn "%s won with an average time of %.2f minutes pr 10 km" winnerName time
+            printfn "-----------------------------------------------------------------------\n"
+        else
+            printfn "-----------------------------------------------------------------------"
+            printfn "No winner, trying again"
+            printfn "-----------------------------------------------------------------------\n"
 main()
